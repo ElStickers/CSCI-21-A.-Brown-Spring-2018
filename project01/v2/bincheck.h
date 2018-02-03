c9bd1e3848fda1e240a1e4f8f3662ea5a28ef90c
Juan Palos <juan.palos@me.com>
2018-02-03 23:43:38 +0000

    Project Luhn Algorithm

---
+++ project01/v2/bincheck.h
@@ -0,0 +1,8 @@
+#ifndef BINCHECK_H
+#define BINCHECK_H
+
+class Bincheck{
+    private:
+        
+}
+
---
+++ project01/v2/testing_Luhn.cpp
@@ -0,0 +1,42 @@
+/*
+ *Name: Luhn Algorithm v2
+ *Author: Juan Palos
+ *Date: 2/1/18
+ *Description: Read credit card numbers from a file and check 
+ *if card is valid and card type. ie.Visa, Mastercard, etc.
+ */
+
+#include "bincheck.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <string>
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::stringstream;
+
+int main(){
+    
+    int card_num, temp;
+    
+    string c_num, line;
+    
+    stringstream ss;
+    
+    ifstream cardin("card_numbers.txt");
+    
+    if (cardin.fail()){
+    cerr << "Error opening/creating a file. Please correct before continuing\n";
+    exit(1);
+  }
+  
+  
+  
+
+}
