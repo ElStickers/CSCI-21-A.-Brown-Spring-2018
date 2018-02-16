516cd4a84c27b9fe0fdb0cf966ea16cbeebcf896
Juan Palos <juan.palos@me.com>
2018-02-16 04:29:35 +0000

    credit machine

---
+++ project01/Final/main.cpp
@@ -0,0 +1,6 @@
+/*
+ * Author:          Juan Palos
+ * Name :           Luhn Algo Project Final
+ * Special Thanks : The one, The only (literally The Only one. Google it.) Mr. Jacob Pawek.
+ * Description :    Reads credit card numbers from a file inputs the info into a or several vectors. Then read in a transaction from a different file 
+ */
